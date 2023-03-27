7dfc3a47a96d8e1fe8cb97ab98538832f4243e54
Legandre <negivj3000@gmail.com>
2023-03-27 22:46:45 +0530

    Dr ONCO App

--- app/src/main/java/com/example/health/splash.kt
+++ app/src/main/java/com/example/health/splash.kt
@@ -9,13 +9,23 @@ import androidx.core.content.ContextCompat
 
 class splash : AppCompatActivity() {
     lateinit var logo : TextView
+    lateinit var logo2 : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         logo =  findViewById(R.id.txtLogo)
+        logo2 =  findViewById(R.id.txtLogo2)
 
+        logo2.alpha = 0f
         logo.alpha = 0f
         logo.animate().setDuration(2000).alpha(1f).withEndAction{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+
+        logo2.animate().setDuration(2000).alpha(1f).withEndAction{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
